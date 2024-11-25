100b98adb67700bd879594b9fd618956da371bd2
Ladislau Lopes <ladislau.contato.01@gmail.com>
2024-11-25 06:01:27 -0500

    andamento no forms

--- central/forms.py
+++ central/forms.py
@@ -1,16 +1,24 @@
 from django import forms
-from .models import MovimentacaoConta, Conta, Categoria
+from .models import MovimentacaoConta, Conta, Categoria, Pessoa
 
 class MovimentacaoContaForm(forms.ModelForm):
     class Meta:
         model = MovimentacaoConta
-        fields = ['titulo', 'valor', 'data', 'conta', 'categoria', 'tipo', 'parcelas', 'valor_parcelas', 'fixo', 'desconto']
+        fields = ['titulo', 'valor', 'data', 'conta', 'categoria', 'tipo', 'parcelado', 'parcelas', 'valor_parcelas', 'fixo', 'desconto']
         widgets = {
             'data': forms.DateInput(attrs={'type': 'date'}),  # Para o campo de data, define um tipo de input de data
         }
 
     def __init__(self, *args, **kwargs):
         super(MovimentacaoContaForm, self).__init__(*args, **kwargs)
-        # Aqui, garantimos que o campo 'desconto' é definido como False por padrão.
+        # Garantindo que o campo 'desconto' seja definido como False por padrão
         if 'instance' not in kwargs:
             self.fields['desconto'].initial = False
+        
+        # Garantindo que o campo 'parcelas' tenha um valor inicial de 1
+        if 'instance' not in kwargs:
+            self.fields['parcelas'].initial = 1
+        
+        # Garantindo que o campo 'valor_parcelas' tenha valor 0 quando 'parcelado' for False
+        if not self.instance.pk or not self.instance.parcelado:
+            self.fields['valor_parcelas'].initial = 0
